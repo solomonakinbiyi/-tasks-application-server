02dfb5689c195c535801798ef6c3f6c01a70803e
solomonakinbiyi <akinbiyisolo22@gmail.com>
2022-11-19 20:59:53 +0100

    table structure created

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,8 +1,43 @@
-import { Entity, PrimaryGeneratedColumn } from "typeorm";
-
+import { Status } from './../../../client/src/components/createTaskForm/enums/Status';
+import { Priority } from './../../../client/src/components/createTaskForm/enums/Priority';
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class Task {
-    @PrimaryGeneratedColumn("uuid")
-    id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    type: 'text',
+  })
+  title: string;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+  })
+  date: string;
+
+  @Column({
+    type: 'longtext',
+  })
+  description: string;
+
+  @Column({
+    type: 'enum',
+    enum: Priority,
+    default: Priority.normal,
+  })
+  priority: Priority;
+
+  @Column({
+    type: 'enum',
+    enum: Status,
+    default: Status.todo,
+  })
+  Status: Status;
 }
