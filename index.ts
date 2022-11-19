cd6a0837fa81f00d61063acedfc33a642c81e80f
solomonakinbiyi <akinbiyisolo22@gmail.com>
2022-11-19 20:36:32 +0100

    added some dependencies

--- index.ts
+++ index.ts
@@ -5,9 +5,15 @@ import express, {
 } from 'express';
 import dotenv from 'dotenv';
 import { DataSource } from 'typeorm';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const app: Express = express();
 dotenv.config();
+
+app.use(bodyParser.json());
+
+app.use(cors())
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
